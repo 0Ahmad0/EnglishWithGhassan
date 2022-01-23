93b919787909355630f240fbd89528a7570e7a80
AHMAD-H <ah774129@gmail.com>
2022-01-23 22:42:20 +0200

    update MEZ Finish

--- lib/screens/video_play_screen.dart
+++ lib/screens/video_play_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_youtube_downloader/flutter_youtube_downloader.dart';
 import 'package:get/get.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
